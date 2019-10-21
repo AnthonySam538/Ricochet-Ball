e8c7761574678b3fa3b99ad9119bfea9626d830d
AnthonySam538 <42751473+AnthonySam538@users.noreply.github.com>
2019-10-21 16:44:34 -0700

    Add files via upload
    Point ball is a struct made up of 2 int variables, causing some inaccuracy.

--- RicochetBallForm.cs
+++ RicochetBallForm.cs
@@ -24,8 +24,8 @@ public class RicochetBallForm : Form
   private const short radius = 5;
   private const double refreshRate = 1000/20;   //20 frames per second (60 would be optimal)
   private const double animationRate = 1000/25; //25 updates per second (should be greater than or equal to refreshRate)
-  // private double distance = 0; //initialize to 0
-  // private double angle = 0; //initialize to 0
+  private double distance;
+  private double angle;
   // private double delta_x;
   // private double delta_y;
 
@@ -43,10 +43,16 @@ public class RicochetBallForm : Form
   private Label info = new Label();
   private Button quitButton = new Button();
 
+  // Create Timers
+  private static System.Timers.Timer refreshClock = new System.Timers.Timer(refreshRate);
+  private static System.Timers.Timer animationClock = new System.Timers.Timer(animationRate);
+
   public RicochetBallForm()
   {
     // Set up texts
+    Text = "Ricochet Ball";
     title.Text = "Ricochet Ball by Anthony Sam";
+    title.TextAlign = ContentAlignment.MiddleCenter;
     newButton.Text = "New";
     distanceText.Text = "Speed: ";
     angleText.Text = "Angle: ";
@@ -61,15 +67,20 @@ public class RicochetBallForm : Form
     angleText.AutoSize = true;
 
     // Set up locations
-    newButton.Location = new Point(formWidth*1/6, formHeight*9/10);
-    distanceBox.Location = new Point(formWidth*2/6, formHeight*9/10);
-    startButton.Location = new Point(formWidth*3/6, formHeight*9/10);
-    info.Location = new Point(formWidth*4/6, formHeight*9/10);
-    quitButton.Location = new Point(formWidth*5/6, formHeight*9/10);
+    newButton.Location = new Point(formWidth*1/6, formHeight*19/20-newButton.Height/2);
+    distanceBox.Location = new Point(formWidth*2/6, formHeight*19/20-distanceBox.Height);
+    distanceText.Location = new Point(distanceBox.Left-distanceText.Width, distanceBox.Top);
+    angleBox.Location = new Point(distanceBox.Left, distanceBox.Bottom);
+    angleText.Location = new Point(angleBox.Left-angleText.Width, angleBox.Top);
+    startButton.Location = new Point(formWidth*3/6, newButton.Top);
+    info.Location = new Point(formWidth*4/6, formHeight*19/20-info.Height/2);
+    quitButton.Location = new Point(formWidth*5/6, newButton.Top);
 
     // Set up colors
     BackColor = Color.Orange;
     title.BackColor = Color.Cyan;
+    distanceText.BackColor = Color.Transparent;
+    angleText.BackColor = Color.Transparent;
     newButton.BackColor = Color.DarkOrchid;
     startButton.BackColor = newButton.BackColor;
     quitButton.BackColor = newButton.BackColor;
@@ -87,6 +98,11 @@ public class RicochetBallForm : Form
     Controls.Add(angleBox);
 
     // Define which method each control should call (The methods will be defined below)
+    newButton.Click += new EventHandler(reset);
+    startButton.Click += new EventHandler(start);
+    quitButton.Click += new EventHandler(quit);
+    refreshClock.Elapsed += new ElapsedEventHandler(refreshScreen);
+    animationClock.Elapsed += new ElapsedEventHandler(updateBall);
   }
 
   // This method draws on the screen
@@ -99,5 +115,41 @@ public class RicochetBallForm : Form
     graphics.FillRectangle(Brushes.Yellow, 0, formHeight*9/10, title.Width, title.Height); //draws the yellow bar for the control panel
 
     base.OnPaint(e);
+  }
+
+  protected void reset(Object sender, EventArgs events)
+  {
+    refreshClock.Stop();
+    animationClock.Stop();
+    ball = new Point(formWidth/2, formHeight/2);
+    info.Text = "X: Y: ";
+    Invalidate();
+  }
+
+  protected void start(Object sender, EventArgs events)
+  {
+    distance = double.Parse(distanceBox.Text);
+    angle = double.Parse(angleBox.Text);
+    refreshClock.Start();
+    animationClock.Start();
+  }
+
+  protected void quit(Object sender, EventArgs events)
+  {
+    System.Console.WriteLine("You clicked on the Quit button.");
+    Close();
+  }
+
+  protected void refreshScreen(Object sender, ElapsedEventArgs events)
+  {
+    info.Text = "X: " + ball.X + " Y: " + ball.Y;
+
+    Invalidate();
+  }
+
+  protected void updateBall(Object sender, ElapsedEventArgs events)
+  {
+    ball.X += (int)(distance*Math.Cos(angle*Math.PI/180));
+    ball.Y -= (int)(distance*Math.Sin(angle*Math.PI/180));
   }
 }
